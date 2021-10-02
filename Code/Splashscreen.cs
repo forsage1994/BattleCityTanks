11839b0dc73ae07feeaa36be2fcad3fbf68a1d2b
forsage1994 <mihal.4sage@gmail.com>
2021-10-02 23:56:27 +0200

    Add project files.

---
+++ Code/Splashscreen.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BattleCityTanks
+{
+  public 
+  static class Splashscreen
+  {
+    public static Texture2D Background { get; set; }
+    //   public static int Multiplication { get; set; }
+    static int yPositionOfBackground = 128;
+//    static private int repeats = 0;
+    static private int backgroundWidth;// = Background.Bounds.Width;
+    static private int backgroundHeight;// = Background.Bounds.Height;
+    static private bool isMove = true;
+
+    static public void MeasureSizeOfTexture()
+    {
+      backgroundWidth = Background.Bounds.Width;
+      backgroundHeight = Background.Bounds.Height;
+    }
+    static public void Draw(SpriteBatch spriteBatch, int multiplication)
+    {
+      //      spriteBatch.Draw(Background, new Vector2(0, repeats), Color.White);
+      backgroundWidth = Background.Bounds.Width;
+      backgroundHeight = Background.Bounds.Height;
+      spriteBatch.Draw(Background, new Rectangle(new Point(0, yPositionOfBackground * multiplication), new Point(backgroundWidth * multiplication, backgroundHeight * multiplication)), Color.White);
+    }
+
+    static public void Update()
+    {
+      if (isMove == true)
+      {
+        if (yPositionOfBackground > 0)
+        {
+          yPositionOfBackground--;
+        } else
+        {
+          isMove = false;
+        }
+//        repeats = 0;
+      } else
+      {
+        
+      }
+    }
+  }
+}
---
+++ Game1.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace BattleCityTanks
+{
+  public class Game1 : Game
+  {
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+    Texture2D YellowTanks;
+    const int OriginalWidth = 256, OriginalHeight = 224;
+    public int CurrentWidth, CurrentHeight, Multiplication;
+
+
+
+    public Game1()
+    {
+      _graphics = new GraphicsDeviceManager(this);
+      Content.RootDirectory = "Content";
+      IsMouseVisible = true;
+
+
+    }
+
+    protected override void Initialize()
+    {
+      CurrentWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+      CurrentHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+
+      if ((CurrentWidth / OriginalWidth) < (CurrentHeight / OriginalHeight))
+        Multiplication = CurrentWidth / OriginalWidth;
+      else
+        Multiplication = CurrentHeight / OriginalHeight;
+
+      _graphics.PreferredBackBufferWidth = OriginalWidth * Multiplication;
+      _graphics.PreferredBackBufferHeight = OriginalHeight * Multiplication;
+      _graphics.IsFullScreen = false;
+      _graphics.ApplyChanges();
+      base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+      _spriteBatch = new SpriteBatch(GraphicsDevice);
+      Splashscreen.Background = Content.Load<Texture2D>("splashscreen");
+      YellowTanks = Content.Load<Texture2D>("yellow_tanks");
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+      if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+        Exit();
+
+      Splashscreen.Update();
+
+      base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+      GraphicsDevice.Clear(Color.Black);
+
+      _spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.PointClamp);
+      //      _spriteBatch.Draw(YellowTanks, new Rectangle(0,0,640,640), Color.White);
+      Splashscreen.Draw(_spriteBatch, Multiplication);
+      _spriteBatch.End();
+
+      base.Draw(gameTime);
+    }
+  }
+}
