1bc2472afefa8dc452264ca73ebb7ec7721b6e70
forsage1994 <mihal.4sage@gmail.com>
2021-10-03 13:17:36 +0200

    Add start menu

--- Code/Splashscreen.cs
+++ Code/Splashscreen.cs
@@ -3,16 +3,33 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace BattleCityTanks
 {
-  public 
+  public
   static class Splashscreen
   {
     public static Texture2D Background { get; set; }
+    public static Texture2D Tank { get; set; }
     //   public static int Multiplication { get; set; }
     static int yPositionOfBackground = 128;
-//    static private int repeats = 0;
+    static private int repeats = 0;
     static private int backgroundWidth;// = Background.Bounds.Width;
     static private int backgroundHeight;// = Background.Bounds.Height;
     static private bool isMove = true;
+    private enum menuItems : byte
+    {
+      OnePlayer = 0,
+      TwoPlayer = 16,
+      Constructor = 32
+    };
+    static menuItems currentItem = menuItems.OnePlayer;
+    //    static int ololo = menuItems.OnePlayer;
+
+    public enum menuItemsChangeDirection
+    {
+      Up,
+      Down
+    }
+
+    static private FrameOfTank currentFrame;
 
     static public void MeasureSizeOfTexture()
     {
@@ -22,27 +39,91 @@ namespace BattleCityTanks
     static public void Draw(SpriteBatch spriteBatch, int multiplication)
     {
       //      spriteBatch.Draw(Background, new Vector2(0, repeats), Color.White);
-      backgroundWidth = Background.Bounds.Width;
-      backgroundHeight = Background.Bounds.Height;
+      //     backgroundWidth = Background.Bounds.Width;
+      //     backgroundHeight = Background.Bounds.Height;
+      MeasureSizeOfTexture();
       spriteBatch.Draw(Background, new Rectangle(new Point(0, yPositionOfBackground * multiplication), new Point(backgroundWidth * multiplication, backgroundHeight * multiplication)), Color.White);
+      if (isMove == false)
+      {
+        //        spriteBatch.Draw(Tank, new Rectangle(new Point(50, 50 + (byte)currentItem), new Point(16 * multiplication, 16 * multiplication)), Color.White);
+        spriteBatch.Draw(Tank, new Rectangle(64 * multiplication, ( 122 + (byte)currentItem ) * multiplication, 16 * multiplication, 16 * multiplication), new Rectangle((int)FrameOfTank.RightFirstFrame, 0, 16, 16), Color.White);
+      }
     }
 
     static public void Update()
     {
+      repeats++;
       if (isMove == true)
       {
         if (yPositionOfBackground > 0)
         {
           yPositionOfBackground--;
-        } else
+        }
+        else
         {
           isMove = false;
         }
-//        repeats = 0;
-      } else
-      {
-        
+        //        repeats = 0;
       }
+      else
+      {
+        if (repeats % 50 == 0) { }
+      }
+    }
+
+    static public void ChangeItem(menuItemsChangeDirection menuItemsChangeDirection)
+    {
+      switch (menuItemsChangeDirection)
+      {
+        case menuItemsChangeDirection.Up:
+          switch (currentItem)
+          {
+            case menuItems.OnePlayer:
+              currentItem = menuItems.OnePlayer;
+              break;
+
+            case menuItems.TwoPlayer:
+              currentItem = menuItems.OnePlayer;
+              break;
+
+            case menuItems.Constructor:
+              currentItem = menuItems.TwoPlayer;
+              break;
+          }
+          break;
+
+        case menuItemsChangeDirection.Down:
+          switch (currentItem)
+          {
+            case menuItems.OnePlayer:
+              currentItem = menuItems.TwoPlayer;
+              break;
+
+            case menuItems.TwoPlayer:
+              currentItem = menuItems.Constructor;
+              break;
+
+            case menuItems.Constructor:
+              currentItem = menuItems.Constructor;
+              break;
+          }
+          break;
+      }
+    }
+    static public Statement SelectItem()
+    {
+      Statement statement = Statement.Game;
+      switch (currentItem)
+      {
+        case menuItems.OnePlayer:
+          statement = Statement.Game;
+          break;
+
+        default:
+
+          break;
+      }
+      return statement;
     }
   }
 }
--- Game1.cs
+++ Game1.cs
@@ -1,9 +1,46 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
 
 namespace BattleCityTanks
 {
+  public enum Statement
+  {
+    Splashscreen,
+    Game,
+    GameOver,
+    Pause
+  }
+  public enum Direction : byte
+  {
+    Up,
+    Down,
+    Left,
+    Right
+  }
+  public enum FrameOfTank : byte
+  {
+    UpFirstFrame = 0,
+    UpSecondFrame = 16,
+    LeftFirstFrame = 32,
+    LeftSecondFrame = 48,
+    DownFirstFrame = 64,
+    DownSecondFrame = 80,
+    RightFirstFrame = 96,
+    RightSecondFrame = 112
+  }
+  public enum TypeOfTank : byte
+  {
+    TypeOne = 0,
+    TypeTwo = 16,
+    TypeThree = 32,
+    TypeFour = 48,
+    TypeFive = 64,
+    TypeSix = 80,
+    TypeSeven = 96,
+    TypeEight = 112
+  }
   public class Game1 : Game
   {
     private GraphicsDeviceManager _graphics;
@@ -11,6 +48,7 @@ namespace BattleCityTanks
     Texture2D YellowTanks;
     const int OriginalWidth = 256, OriginalHeight = 224;
     public int CurrentWidth, CurrentHeight, Multiplication;
+    public Statement statement = Statement.Splashscreen;
 
 
 
@@ -20,18 +58,20 @@ namespace BattleCityTanks
       Content.RootDirectory = "Content";
       IsMouseVisible = true;
 
-
-    }
-
-    protected override void Initialize()
-    {
       CurrentWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
       CurrentHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+      Debug.WriteLine(CurrentWidth.ToString() + "x" + CurrentHeight.ToString());
 
       if ((CurrentWidth / OriginalWidth) < (CurrentHeight / OriginalHeight))
         Multiplication = CurrentWidth / OriginalWidth;
       else
         Multiplication = CurrentHeight / OriginalHeight;
+
+      
+    }
+
+    protected override void Initialize()
+    {
 
       _graphics.PreferredBackBufferWidth = OriginalWidth * Multiplication;
       _graphics.PreferredBackBufferHeight = OriginalHeight * Multiplication;
@@ -44,6 +84,7 @@ namespace BattleCityTanks
     {
       _spriteBatch = new SpriteBatch(GraphicsDevice);
       Splashscreen.Background = Content.Load<Texture2D>("splashscreen");
+      Splashscreen.Tank = Content.Load<Texture2D>("yellow_tanks");
       YellowTanks = Content.Load<Texture2D>("yellow_tanks");
     }
 
@@ -52,6 +93,39 @@ namespace BattleCityTanks
       if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
         Exit();
 
+      switch (statement)
+      {
+        case Statement.Splashscreen:
+
+          if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Up))
+          {
+            Splashscreen.ChangeItem(Splashscreen.menuItemsChangeDirection.Up);
+          }
+
+          if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Down))
+          {
+            Splashscreen.ChangeItem(Splashscreen.menuItemsChangeDirection.Down);
+          }
+
+          if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Enter))
+          {
+            statement = Splashscreen.SelectItem();
+          }
+
+          break;
+
+        case Statement.Game:
+
+          break;
+
+        case Statement.GameOver:
+
+          break;
+
+        case Statement.Pause:
+
+          break;
+      }
       Splashscreen.Update();
 
       base.Update(gameTime);
@@ -62,8 +136,26 @@ namespace BattleCityTanks
       GraphicsDevice.Clear(Color.Black);
 
       _spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.PointClamp);
-      //      _spriteBatch.Draw(YellowTanks, new Rectangle(0,0,640,640), Color.White);
-      Splashscreen.Draw(_spriteBatch, Multiplication);
+
+      switch (statement)
+      {
+        case Statement.Splashscreen:
+          Splashscreen.Draw(_spriteBatch, Multiplication);
+          break;
+
+        case Statement.Game:
+
+          break;
+
+        case Statement.GameOver:
+
+          break;
+
+        case Statement.Pause:
+
+          break;
+      }
+
       _spriteBatch.End();
 
       base.Draw(gameTime);
